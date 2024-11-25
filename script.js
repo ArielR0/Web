8d21259ee7a732b2d34b2986ffcf31b1b7f1906f
Ariel R Santos! <arielsantos9086@gmail.com>
2024-11-24 23:58:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.inicio-text', {duration: 2500});
+
+sr.reveal(".img",{
+    rotate: {x:100, y:100, z:100}, duration: 900,
+});
+
+sr.reveal('.add-text', {duration: 1500});
+
+sr.reveal('.icon', {duration: 1500});
+
+sr.reveal('.info h1', {duration: 1500});
+
+sr.reveal('.info p', {duration: 1500});
+
+sr.reveal('.info button', {duration: 1500});
+
+sr.reveal('.ing', {duration: 1500});
