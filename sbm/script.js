bb45ec7d8f8e7d0b4a1531375901439c7a2c1da0
ArielR0 <arielsantos9086@gmail.com>
2024-12-04 13:38:30 -0300

    JS

---
+++ sbm/script.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.menu', {duration: 2500});
+
+sr.reveal(".img",{ duration: 3000,
+});
+
+sr.reveal('.Trapped ', {duration: 1500});
+
+sr.reveal('.Destroyer', {duration: 2500});
+
+sr.reveal('#Container', {duration: 2500});
+
+sr.reveal('.Storm', {duration: 1500});
+
+sr.reveal('.Storm video', {duration: 2500});
+
+sr.reveal('.Trapped video', {duration: 2500});
